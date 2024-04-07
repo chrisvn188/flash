58487377334ef503a66aa6f137771e05e1c51dd7
chrisvn188 <chrisbui.dev188@gmail.com>
2024-04-07 09:59:09 -0400

    Add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+
+    entry: {
+        main: './src/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+        ],
+    },
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        static: './dist',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            body: true,
+        }),
+    ],
+}
